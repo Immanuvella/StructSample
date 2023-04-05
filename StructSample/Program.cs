5a11eb439f5391b7e669f2cf01338d8a83b474a8
Immanuvella <paulsheerima@gmail.com>
2023-04-05 22:15:13 +0530

    Add project files.

---
+++ StructSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//sample program to understand the concept of struct
+namespace StructSample
+{
+    struct Book//created a structure called Book
+    {
+        public int id;
+        public string title;
+       
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[10];
+            Console.WriteLine(  "Enter the number of Books :");
+            int n=int.Parse(Console.ReadLine());
+            for (int i=0;i<n;i++)
+            {
+                Console.WriteLine(  "Enter Book Id :");
+                books[i].id = int.Parse(Console.ReadLine());
+                Console.WriteLine(  "Enter Book name :");
+                books[i].title = Console.ReadLine();
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine( "Id = {0}, Author = {1}", books[i].id, books[i].title);
+            }
+            Console.ReadKey();
+        }
+    }
+}
